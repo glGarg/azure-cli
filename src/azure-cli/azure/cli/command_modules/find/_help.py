1e697959c1825e449ffd97c2c0a23a4ae0b89ebc
t-spga <t-spga@DESKTOP-H29VM0V>
2019-09-09 16:51:40 -0700

    Add new examples to find 	- Add new examples to the following modules: 		find

--- src/azure-cli/azure/cli/command_modules/find/_help.py
+++ src/azure-cli/azure/cli/command_modules/find/_help.py
@@ -8,16 +8,4 @@ from knack.help_files import helps  # pylint: disable=unused-import
 # pylint: disable=line-too-long, too-many-lines
 
 helps['find'] = """
-type: command
-short-summary: I'm an AI robot, my advice is based on our Azure documentation as well as the usage patterns of Azure CLI and Azure ARM users. Using me improves Azure products and documentation.
-examples:
-  - name: Give me any Azure CLI group and I’ll show the most popular commands within the group.
-    text: |
-        az find "az storage"
-  - name: Give me any Azure CLI command and I’ll show the most popular parameters and subcommands.
-    text: |
-        az find "az monitor activity-log list"
-  - name: You can also enter a search term, and I'll try to help find the best commands.
-    text: |
-        az find "arm template"
-"""
+b'type: command\nshort-summary: I\'m an AI robot, my advice is based on our Azure documentation as well as the usage patterns of Azure CLI and Azure ARM users. Using me improves Azure products and documentation.\nexamples:\n  - name: Give me any Azure CLI group and I\xe2\x80\x99ll show the most popular commands within the group.\n    text: |\n        az find "az storage"\n  - name: Give me any Azure CLI command and I\xe2\x80\x99ll show the most popular parameters and subcommands.\n    text: |\n        az find "az monitor activity-log list"\n  - name: You can also enter a search term, and I\'ll try to help find the best commands.\n    text: |\n        az find "arm template"\n'"""
