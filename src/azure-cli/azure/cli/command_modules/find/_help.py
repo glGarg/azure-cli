91a8715cb4b1b6d8a9f3c8f21c2bdaad05a02e94
t-spga <t-spga@DESKTOP-H29VM0V>
2019-09-15 11:13:11 -0700

    Sort commands in find _help.py 	- Sort commands in help files for easier diffs in the future.

--- src/azure-cli/azure/cli/command_modules/find/_help.py
+++ src/azure-cli/azure/cli/command_modules/find/_help.py
@@ -4,9 +4,11 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from knack.help_files import helps
+from knack.help_files import helps  # pylint: disable=unused-import
+# pylint: disable=line-too-long, too-many-lines
 
 helps['find'] = """
+
     type: command
     short-summary: I'm an AI robot, my advice is based on our Azure documentation as well as the usage patterns of Azure CLI and Azure ARM users. Using me improves Azure products and documentation.
     examples:
@@ -19,4 +21,5 @@ helps['find'] = """
         - name: You can also enter a search term, and I'll try to help find the best commands.
           text: |
             az find "arm template"
+ 
 """
